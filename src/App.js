052583e140caed11420ff8d0f15c90a089fdb63c
Lex Christopherson <lexchristopherson@Lexs-MacBook-Pro.local>
2024-10-23 00:44:24 -0600

    Added meta pixel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Card from './components/ui/Card';
 import Button from './components/ui/Button';
 import myPhoto from './assets/photo.webp';
@@ -55,6 +55,38 @@ const features = [
 ];
 
 export default function MasterclassLanding() {
+
+  // Add Meta Pixel useEffect here
+  useEffect(() => {
+    const pixelId = process.env.REACT_APP_PIXEL_ID;
+    
+    if (!pixelId) {
+      console.error("Pixel ID is not defined in .env");
+      return;
+    }
+
+    (function(f,b,e,v,n,t,s) {
+      if(f.fbq) return; 
+      n = f.fbq = function() {
+        n.callMethod ? n.callMethod.apply(n,arguments) : n.queue.push(arguments);
+      };
+      if(!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = '2.0';
+      n.queue = [];
+      t = b.createElement(e); 
+      t.async = !0;
+      t.src = v; 
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s);
+    })(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+    window.fbq('init', pixelId); 
+    window.fbq('track', 'PageView');
+    
+  }, []);
+
   return (
     <div style={{ backgroundColor: '#0D0C0D' }} className="min-h-screen text-white">
       {/* Hero Section */}
